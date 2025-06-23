177111d5f0a64b0bb9744b3ca08e1a8c02e13edd
Ezequiel Mónaco <ezmonaco@uade.edu.ar>
2025-06-22 21:22:27 -0300

    agrego aleatorio a frontend y cargo selects en base a backend

--- frontend/src/types/common.ts
+++ frontend/src/types/common.ts
@@ -9,4 +9,9 @@ export interface SelectOptions {
   mood: SelectOption[];
   instrument: SelectOption[];
   gender: SelectOption[];
-} 
+}
+
+export interface HistoryItem {
+  prompt: string;
+  fecha_introduccion: string;
+}
